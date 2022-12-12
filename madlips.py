3d8abaaabc45e72cb17340e7667ca83288e3762b
Frankmona <frankoguche1@.com>
2022-12-12 15:19:24 +0100

    madlip project(about)

---
+++ madlips.py
@@ -0,0 +1,26 @@
+#string concatenation(how to add strings together)
+# Ways to do string concatenation
+# print("subscribe to " + youtuber)
+#print(f"subscribe to {youtuber}")
+
+name = input("name: ")
+discipline = input("discipline: " )
+university = input("university: ")
+state = input("state: ")
+country =input("country: ")
+Job_title = input("Job_title: ")
+Company = input("company: ")
+skills1 = input("skill: ")
+skills2 = input("skill: ")
+skills3 = input("skill: ")
+
+
+About=  F" My name is {name},i am a grduate of {discipline} from {university}, {state}, {country} \
+    Currently, i am a {Job_title} at  {Company} \
+        Am a fast learner, with excellent problem solving capabilities, good communication, {skills1}, {skills2}, {skills3} \
+    More so, i have impressive track record of succes which has made organization stand out."      
+print(About)
+
+
+
+    
